6f89b0488e698b0260902cd54e1b762d7643aefd
SRVignesh2000 <vignesh.ramachandran@fwsa.freshworks.com>
2023-02-02 15:11:43 +0530

    url parameter added

--- Javascript/reverse.js
+++ Javascript/reverse.js
@@ -1,18 +1,18 @@
-let n = 5;
-let x = "";
-for (i=1; i<=5; i++)
-{
-    for(j=1; j<=i; j++)
-    {
-        x += ".";
-    }
-    for(k=n-i+1; k>=1; k--)
-    {
-        x += k;
-    }
-    x += "\n";
-}
-    console.log(x);
+// let n = 5;
+// let x = "";
+// for (i=1; i<=5; i++)
+// {
+//     for(j=1; j<=i; j++)
+//     {
+//         x += ".";
+//     }
+//     for(k=n-i+1; k>=1; k--)
+//     {
+//         x += k;
+//     }
+//     x += "\n";
+// }
+//     console.log(x);
 
 
 // let n = 5;
@@ -31,3 +31,18 @@ for (i=1; i<=5; i++)
 //   console.log(x);
 
 
+//   let n = 4;
+// let string = "";
+// for (let i = 1; i <= n; i++) {
+//     string+=i;
+//     string+="\n";
+//         for(let j=1; j<=i; j++){
+//         string+="."+ "." ;}
+// }
+// for (let j = n-1; j >= 1; j--) {
+//     string+=j;
+//     string+="\n";
+//         for(let k=n+3; k>=j; k--){
+//         string+="."+ "." ;}
+// }
+//     console.log(string);
