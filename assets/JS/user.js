b475db1d66023bc5f7b08943530e546880d3bbcb
SRVignesh2000 <vignesh.ramachandran@fwsa.freshworks.com>
2023-05-21 17:22:00 +0530

    feat: myorders css done

--- assets/JS/user.js
+++ assets/JS/user.js
@@ -1,25 +1,23 @@
-class User{
-    name
-    email
-    phoneNumber
-    password
+// class User{
+//     name
+//     email
+//     phoneNumber
+//     password
 
-    validateFirstName(name) {
-        const pattern = new RegExp(/[a-z]/)
-        return pattern.test(firstName)
-    }
+//     validateFirstName(name) {
+//         const pattern = new RegExp(/[a-z]/)
+//         return pattern.test(firstName)
+//     }
 
-    validateLastName(email) {
-        const pattern = new RegExp(/[]/)
-        return pattern.test(email)
-    }
+//     validateLastName(email) {
+//         const pattern = new RegExp(/[]/)
+//         return pattern.test(email)
+//     }
 
-    validatePhoneNumber(phoneNumber) {
-        const pattern = new RegExp(/[6-9]{1}[0-9]{9}/)
-        return pattern.test(phoneNumber)
-    }
-}
+//     validatePhoneNumber(phoneNumber) {
+//         const pattern = new RegExp(/[6-9]{1}[0-9]{9}/)
+//         return pattern.test(phoneNumber)
+//     }
+// }
 
-
-const user = new User();
-
+// const user = new User();
